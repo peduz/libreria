d711a8b179468a597fbd7e0fc685947650e88f1d
Giuseppe Pedulla <gpedulla@it-present.com>
2024-06-29 10:53:08 +0200

    Aggiunti endpoint

--- src/main/java/it/gpedulla/libreria/controller/BookController.java
+++ src/main/java/it/gpedulla/libreria/controller/BookController.java
@@ -1,12 +1,15 @@
 package it.gpedulla.libreria.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.gpedulla.libreria.model.Book;
 import it.gpedulla.libreria.repository.BookRepository;
@@ -19,13 +22,28 @@ public class BookController {
 	private BookRepository repository;
 	
 	@GetMapping
-	public String index(Model model) {
+	public String index(Model model, 
+			@RequestParam(name = "author", required = false) String author) {
 		
-		List<Book> libri = repository.findAll();
+		List<Book> libri = new ArrayList<>();
+		
+		if(author == null || author.isBlank()) {
+			libri = repository.findAll();
+		} else {
+			libri = repository.findByAuthorOrderByIdDesc(author);
+		}
 		
 		model.addAttribute("list", libri);
 		
 		return "/books/index";
+	}
+	
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") Integer bookId, Model model) {
+		
+		model.addAttribute("book", repository.getReferenceById(bookId));
+		
+		return "/books/show";
 	}
 	
 }
--- src/main/java/it/gpedulla/libreria/repository/BookRepository.java
+++ src/main/java/it/gpedulla/libreria/repository/BookRepository.java
@@ -1,9 +1,20 @@
 package it.gpedulla.libreria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.gpedulla.libreria.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
+	/*
+	 * SELECT *
+	 * FROM book b
+	 * WHERE b.author = ?
+	 */
+	public List<Book> findByAuthor(String author);
+	
+
+	public List<Book> findByAuthorOrderByIdDesc(String author);
 }
