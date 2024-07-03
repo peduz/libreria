d4eb56d42de1c32a7cd03cb607e75b323ae60df6
Giuseppe Pedulla <gpedulla@it-present.com>
2024-07-03 11:51:12 +0200

    Aggiunta funzinalità

--- src/main/java/it/gpedulla/libreria/controller/BookController.java
+++ src/main/java/it/gpedulla/libreria/controller/BookController.java
@@ -6,13 +6,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.gpedulla.libreria.model.Book;
 import it.gpedulla.libreria.repository.BookRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/books")
@@ -47,4 +51,26 @@ public class BookController {
 		return "/books/show";
 	}
 	
+	@GetMapping("/create")
+	public String create(Model model) {
+
+	    model.addAttribute("book", new Book());
+	    
+	    return "/books/create";
+	}
+	
+	@PostMapping("/create")
+	public String store(
+		@Valid @ModelAttribute("book") Book formBook,
+	   BindingResult bindingResult,
+	   Model model){
+		
+	   if(bindingResult.hasErrors()) {
+	      return "/books/create";
+	   }
+
+	   repository.save(formBook);
+
+	   return "redirect:/books";
+	}
 }
--- src/main/java/it/gpedulla/libreria/model/Book.java
+++ src/main/java/it/gpedulla/libreria/model/Book.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "book")
@@ -15,14 +17,18 @@ public class Book {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank(message = "Title cannot be null")
 	private String title;
 
+	@NotBlank(message = "Author cannot be null")
 	@Column(name="author")
 	private String author;
 	
+	@NotBlank(message = "isbn cannot be null")
+	@Size(min = 13, max = 13, message = "isbn must have 13 chars")
 	@Column(name="isbn_code", nullable=false, unique=true)
 	private String isbn;
-
+	
 	public Integer getId() {
 		return id;
 	}
