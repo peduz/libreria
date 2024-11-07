d9e4a17717c1c7cf9d7bcbb14826fe789d2b8cfc
Giuseppe Pedullà <peduz91@gmail.com>
2024-11-07 21:08:54 +0100

    Aggiunta show, filter e collegamento con la navbar

--- src/main/java/it/lessons/libreria/controller/BooksController.java
+++ src/main/java/it/lessons/libreria/controller/BooksController.java
@@ -1,12 +1,15 @@
 package it.lessons.libreria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.lessons.libreria.model.Book;
 import it.lessons.libreria.repository.BookRepository;
@@ -17,13 +20,40 @@ public class BooksController {
 
 	@Autowired
 	private BookRepository bookRepo;
-	
+
 	@GetMapping
-	public String index(Model model) {
-		List<Book> allBooks = bookRepo.findAll();
+	public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
 		
+		List<Book> allBooks;
+		
+		if(keyword != null && !keyword.isBlank()) {
+			allBooks = bookRepo.findByTitleContaining(keyword);
+			model.addAttribute("keyword", keyword);
+		} else {
+			allBooks = bookRepo.findAll();
+		}
+		
+
 		model.addAttribute("books", allBooks);
-		
+
 		return "books/index";
+	}
+
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable(name = "id") Long id, @RequestParam(name = "keyword", required = false) String keyword,
+			Model model) {
+
+		Optional<Book> bookOptional = bookRepo.findById(id);
+
+		if (bookOptional.isPresent()) {
+			model.addAttribute("book", bookOptional.get());
+		}
+		model.addAttribute("keyword", keyword);
+		if(keyword == null || keyword.isBlank() || keyword.equals("null")) {
+			model.addAttribute("bookUrl", "/books");
+		} else {			
+			model.addAttribute("bookUrl", "/books?keyword=" + keyword);
+		}
+		return "books/show";
 	}
 }
--- src/main/java/it/lessons/libreria/repository/BookRepository.java
+++ src/main/java/it/lessons/libreria/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package it.lessons.libreria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.lessons.libreria.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	
+	public List<Book> findByTitleContaining(String title);
 }
